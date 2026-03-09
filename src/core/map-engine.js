4b5dc4146c14db02e013db0aa1ef9d56c8ce09c0
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-09 18:47:32 +0300

    Update map-engine.js

--- src/core/map-engine.js
+++ src/core/map-engine.js
@@ -1,1 +1,30 @@
+/**
+ * Map Engine - Harita Çekirdeği
+ */
+export const MapEngine = {
+    map: null,
+
+    /**
+     * Haritayı başlatır
+     * @param {string} containerId - Haritanın yükleneceği HTML element ID'si
+     */
+    init(containerId) {
+        mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+        this.map = new mapboxgl.Map({
+            container: containerId,
+            style: 'mapbox://styles/mapbox/dark-v11',
+            center: [35, 39], // Türkiye merkezli başlangıç
+            zoom: 4,
+            projection: 'globe'
+        });
+
+        this.map.on('style.load', () => {
+            this.map.setFog({}); // Atmosfer efekti
+        });
+
+        console.log("Map Engine başlatıldı.");
+        return this.map;
+    }
+};
 
