51c58634603a0fc92292d6563e928a2397c7c846
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-09 20:48:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,1 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // index.html artık ana dizinde olduğu için root'u siliyoruz veya '.' yapıyoruz
-  base: '/SEISMO/',
-  build: {
-    // Çıktı klasörü artık kök dizine göre 'dist' olmalı
-    outDir: 'dist',
-    emptyOutDir: true,
-    // Dosya yollarının doğru çözümlenmesi için rollup ayarı (isteğe bağlı)
-    rollupOptions: {
-      input: './index.html'
-    }
-  }
-});
 
