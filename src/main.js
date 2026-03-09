3be560f1892b75d6dc59f3170a3849fe8beb19ea
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-09 15:53:31 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log("SeismoPro sistemi başlatıldı...");
+
+document.getElementById('app').innerHTML = `
+    <h1 style="color: white; text-align: center; margin-top: 50px;">
+        Sistem Yükleniyor...
+    </h1>
+`;
+
