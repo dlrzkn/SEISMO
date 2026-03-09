9de89f657b02328a33868ebb11d1deabc41f6541
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-09 16:05:13 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import './css/style.css';
 import { MapManager } from './core/map';
+import { EarthquakeService } from './services/earthquake'; // Yeni eklenen satır
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGxyemtuIiwiYSI6ImNtbWY2ZG5pNDA0cmwycnNodm1jdTN3cmQifQ.Sf5rAPwn1JZfwpDF_blj8Q';
 
 const app = {
     async init() {
-        // HTML yapısını oluştur
         document.getElementById('app').innerHTML = `
             <div id="map" style="width: 100vw; height: 100vh;"></div>
         `;
 
-        // Harita modülünü başlat
         this.mapManager = new MapManager('map', MAPBOX_TOKEN);
         this.mapManager.init();
 
-        console.log("Harita modülü başarıyla yüklendi.");
+        // Veri servisini başlat ve test et
+        this.earthquakeService = new EarthquakeService();
+        const data = await this.earthquakeService.fetchAll();
+        console.log("Gelen Ham Veri:", data);
     }
 };
 
