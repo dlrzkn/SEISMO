654866b5d1f364e542af85a9ab1e1be975423483
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-09 16:09:00 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,28 @@
 import './css/style.css';
 import { MapManager } from './core/map';
-import { EarthquakeService } from './services/earthquake'; // Yeni eklenen satır
+import { EarthquakeService } from './services/earthquake';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGxyemtuIiwiYSI6ImNtbWY2ZG5pNDA0cmwycnNodm1jdTN3cmQifQ.Sf5rAPwn1JZfwpDF_blj8Q';
 
 const app = {
     async init() {
+        // 1. Ekran yapısını oluştur
         document.getElementById('app').innerHTML = `
             <div id="map" style="width: 100vw; height: 100vh;"></div>
         `;
 
+        // 2. Haritayı başlat
         this.mapManager = new MapManager('map', MAPBOX_TOKEN);
         this.mapManager.init();
 
-        // Veri servisini başlat ve test et
+        // 3. Veri servisini başlat
         this.earthquakeService = new EarthquakeService();
-        const data = await this.earthquakeService.fetchAll();
-        console.log("Gelen Ham Veri:", data);
+        
+        // 4. Verileri çek ve haritayı güncelle
+        const events = await this.earthquakeService.fetchAll();
+        this.mapManager.updateData(events);
+
+        console.log(`${events.length} adet deprem verisi haritaya işlendi.`);
     }
 };
 
